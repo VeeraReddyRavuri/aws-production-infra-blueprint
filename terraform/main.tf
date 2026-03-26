564318621244889e5a212c91f997d685069cd640
VeeraReddyRavuri <veerareddy.ravuri01@gmail.com>
2026-03-26 03:00:53 +0000

    Add private EC2 and configure NAT instance routing

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,10 +71,10 @@ resource "aws_security_group" "nat_sg" {
     vpc_id = aws_vpc.main.id
 
     ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["10.0.2.0/24"]
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
@@ -94,6 +94,8 @@ resource "aws_instance" "nat" {
     associate_public_ip_address = true
 
     source_dest_check = false
+
+    key_name = aws_key_pair.main.key_name
 
     tags = {
       Name = "nat-instance"
@@ -120,4 +122,22 @@ resource "aws_route" "private_internet" {
 resource "aws_route_table_association" "private_association" {
     subnet_id = aws_subnet.private.id
     route_table_id = aws_route_table.private.id  
+}
+
+# TO connect to NAT instance, we need ssh access, so we add key pair
+resource "aws_key_pair" "main" {
+    key_name = "devops-key"
+    public_key = file("/home/veera/.ssh/id_rsa.pub")
+}
+
+# Create a Private EC2 instance
+resource "aws_instance" "private_ec2" {
+    ami = "ami-0f5ee92e2d63afc18"
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.private.id
+    vpc_security_group_ids = [aws_security_group.nat_sg.id]
+
+    tags = {
+      Name = "private-ec2"
+    }
 }
