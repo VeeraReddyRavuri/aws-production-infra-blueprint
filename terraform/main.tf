68aa6a7cd8e9809e70a4b16e25a1b6539aaade27
VeeraReddyRavuri <veerareddy.ravuri01@gmail.com>
2026-03-26 08:15:25 +0000

    fix: update NAT SG to allow VPC traffic for proper routing

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,6 +71,13 @@ resource "aws_security_group" "nat_sg" {
     vpc_id = aws_vpc.main.id
 
     ingress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["10.0.0.0/16"]
+    }
+
+    ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
@@ -130,14 +137,69 @@ resource "aws_key_pair" "main" {
     public_key = file("/home/veera/.ssh/id_rsa.pub")
 }
 
+resource "aws_security_group" "private_sg" {
+    name = "private-sg"
+    vpc_id = aws_vpc.main.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        security_groups = [aws_security_group.bastion_sg.id]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 # Create a Private EC2 instance
 resource "aws_instance" "private_ec2" {
     ami = "ami-0f5ee92e2d63afc18"
     instance_type = "t3.micro"
     subnet_id = aws_subnet.private.id
-    vpc_security_group_ids = [aws_security_group.nat_sg.id]
+    vpc_security_group_ids = [aws_security_group.private_sg.id]
+    key_name = aws_key_pair.main.key_name
 
     tags = {
       Name = "private-ec2"
+    }
+}
+
+# Create Bastion Host
+resource "aws_security_group" "bastion_sg" {
+    name = "bastion-sg"
+    description = "Allow SSH from your IP"
+    vpc_id = aws_vpc.main.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+# Create a bstion instance
+resource "aws_instance" "bastion" {
+    ami = "ami-0f5ee92e2d63afc18"
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.public.id
+    vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+    associate_public_ip_address = true
+    key_name = aws_key_pair.main.key_name
+
+    tags = {
+        Name = "bastion-host"
     }
 }
